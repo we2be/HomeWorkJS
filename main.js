bae4c4b26dcfa9dbac6ac56c46e314d3ad00c602
we2be <we2be2@yandex.ru>
2023-01-11 14:07:14 +0300

    HomeWork

---
+++ main.js
@@ -0,0 +1,15 @@
+// let a = parseInt(promt("Введите температуру в градусах Цельсия"));
+// alert(`по Фарингейту получается ${a} равен ${a=(9/5) * градусы Цельсия + 32 }`);
+// function a=(9/5)*a+32{
+//     return
+// }
+
+// const CelsiusT = +prompt(`Темп градусов в Цельсия`);
+// const FarenheitT = (9 / 5) * CelsiusT + 32;
+// alert("В цельсий: " + CelsiusT + ", В фаренгейт: " + FarenheitT);
+
+const UserName = prompt("Введите имя");
+function greeting(UserName) {
+    console.log("Привет, " + UserName + "!");
+}
+greeting(UserName);
