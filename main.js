700ba13eecf8f0b11d83626b69b9af16c12a2465
we2be <we2be2@yandex.ru>
2023-01-11 17:44:21 +0300

    Изменил немного

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-// let a = parseInt(promt("Введите температуру в градусах Цельсия"));
-// alert(`по Фарингейту получается ${a} равен ${a=(9/5) * градусы Цельсия + 32 }`);
-// function a=(9/5)*a+32{
-//     return
+const CelsiusT = +prompt(`Темп градусов в Цельсия`);
+const FarenheitT = (9 / 5) * CelsiusT + 32;
+alert("В цельсий: " + CelsiusT + ", В фаренгейт: " + FarenheitT);
+
+// const UserName = prompt("Введите имя");
+// function greeting(UserName) {
+//     console.log("Привет, " + UserName + "!");
 // }
-
-// const CelsiusT = +prompt(`Темп градусов в Цельсия`);
-// const FarenheitT = (9 / 5) * CelsiusT + 32;
-// alert("В цельсий: " + CelsiusT + ", В фаренгейт: " + FarenheitT);
-
-const UserName = prompt("Введите имя");
-function greeting(UserName) {
-    console.log("Привет, " + UserName + "!");
-}
-greeting(UserName);
+// greeting(UserName);
